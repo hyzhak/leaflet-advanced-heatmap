197b625eed1c32e94f3bfa10a45981ba031f6883
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2019-01-31 11:00:04 +0100

    exclude leaflet as peer dependency

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import clear from 'rollup-plugin-clear'
 import commonjs from 'rollup-plugin-commonjs'
 import license from 'rollup-plugin-license'
 import resolve from 'rollup-plugin-node-resolve'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 import pkg from './package.json'
 
@@ -21,6 +22,7 @@ module.exports = [
         targets: ['dist']
       }),
       resolve(),
+      peerDepsExternal(),
       commonjs(),
       babel({
         exclude: ['node_modules/**']
@@ -45,6 +47,7 @@ module.exports = [
     ],
     plugins: [
       resolve(),
+      peerDepsExternal(),
       commonjs(),
       babel({
         exclude: ['node_modules/**']
